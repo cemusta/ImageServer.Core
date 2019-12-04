68d0c37945ffe9536651e09805343283dbde5737
Cem Usta <nooneelse@gmail.com>
2019-12-04 17:14:09 +0100

    code smell fixed.

--- ImageServer.Core/Services/FileAccess/WebAccess.cs
+++ ImageServer.Core/Services/FileAccess/WebAccess.cs
@@ -12,33 +12,27 @@ namespace ImageServer.Core.Services.FileAccess
         public async Task<byte[]> GetFileAsync(HostConfig host, string file)
         {
             var url = $"{host.Backend}/{file}";
-            try
-            {
-                using var stream = new MemoryStream();
-                using var client = new HttpClient();
-                using var request = new HttpRequestMessage(HttpMethod.Get, url);
-                using var response = await client.SendAsync(request);
+
+            using var stream = new MemoryStream();
+            using var client = new HttpClient();
+            using var request = new HttpRequestMessage(HttpMethod.Get, url);
+            using var response = await client.SendAsync(request);
                 
-                if (response.StatusCode == HttpStatusCode.NotFound)
-                {
-                    throw new FileNotFoundException("File not found", url);
-                }
-
-                if (response.StatusCode != HttpStatusCode.OK)
-                {
-                    throw new HttpRequestException($"Http request not OK: {response.StatusCode}, url: {url}");
-                }
-
-                using var contentStream = await response.Content.ReadAsStreamAsync();
-
-                await contentStream.CopyToAsync(stream);
-
-                return stream.TryGetBuffer(out ArraySegment<byte> data) ? data.Array : null;
-            }
-            catch
+            if (response.StatusCode == HttpStatusCode.NotFound)
             {
-                throw;
+                throw new FileNotFoundException("File not found", url);
             }
+
+            if (response.StatusCode != HttpStatusCode.OK)
+            {
+                throw new HttpRequestException($"Http request not OK: {response.StatusCode}, url: {url}");
+            }
+
+            using var contentStream = await response.Content.ReadAsStreamAsync();
+
+            await contentStream.CopyToAsync(stream);
+
+            return stream.TryGetBuffer(out ArraySegment<byte> data) ? data.Array : null;
         }
     }
 }
